874c8bae7245dd1782707a9615431f6beaffa1ab
haejin.yang91 <haejin.yang91@gmail.com>
2023-05-06 03:47:34 +0900

    feat: 검색 결과 구현 첫 번째 요구사항

--- vanilla/src/controllers/MainController.js
+++ vanilla/src/controllers/MainController.js
@@ -1,4 +1,6 @@
 import FormView from "../views/FormView.js";
+import ResultView from "../views/ResultView.js";
+import SearchModel from "../models/SearchModel.js";
 
 const tag = '[MainController]';
 
@@ -9,13 +11,28 @@ export default {
       FormView.setup(document.querySelector('form'))
           .on('@submit', e => this.onSubmit(e.detail.input))
           .on('@reset', e => this.onRestForm());
+
+      ResultView.setup(document.querySelector('#search-result'));
+    },
+
+    search(query) {
+      console.log(tag, 'search()', query);
+      SearchModel.list("").then(data => {
+          this.onSearchResult(data);
+      });
+      //search api
     },
 
     onSubmit(input) {
         console.log(tag, 'onSubmit()', input);
+        this.search(input);
     },
 
     onRestForm() {
         console.log(tag, 'onRestForm()');
+    },
+
+    onSearchResult(data) {
+        ResultView.render(data);
     }
 }
