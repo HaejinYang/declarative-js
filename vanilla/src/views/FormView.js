6dcd7d932851292981dd86b328be7b19345197d0
haejin.yang91 <haejin.yang91@gmail.com>
2023-05-06 01:49:47 +0900

    feat: 검색폼 구현 두 번째 요구사항

--- vanilla/src/views/FormView.js
+++ vanilla/src/views/FormView.js
@@ -9,10 +9,33 @@ FormView.setup = function (el) {
     this.inputEl = el.querySelector('[type=text]');
     this.resetEl = el.querySelector('[type=reset]');
     this.showResetButton(false);
+    this.registerOnInputHandler();
+    this.registerOnSubmitHandler();
 }
 
 FormView.showResetButton = function (isShow = true) {
     this.resetEl.style.display = isShow ? 'block' : 'none';
 }
+
+FormView.registerOnInputHandler = function () {
+    this.on('input', e => {
+        // 2. 검색어를 보이면 x버튼이 보이고, 없으면 x버튼을 숨긴다.
+        this.showResetButton();
+    });
+}
+
+FormView.registerOnSubmitHandler = function () {
+    this.on('submit', e => {
+        console.log(tag, 'registerOnResetHandler()');
+        e.preventDefault();
+        this.showResetButton(false);
+        this.clearInput();
+    });
+}
+
+FormView.clearInput = function () {
+    this.inputEl.value = "";
+}
+
 
 export default FormView;
